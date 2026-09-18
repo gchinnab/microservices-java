47eb3a4e5accd142c479e72b169b59345b52d8c0
gchinnab <createrbrahma@gmail.com>
2026-09-18 23:48:39 +0530

    code changes as per mysql db, docker-compose

---
+++ section7/configserver/src/test/java/com/chinna/learn/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinna.learn.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
